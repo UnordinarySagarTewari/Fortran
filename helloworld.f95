f03b171a40c1558fcd4e782e86aff7b07bde5c97
UnordinarySagarTewari <72225921+UnordinarySagarTewari@users.noreply.github.com>
2022-01-29 19:40:04 +0530

    fortran file

---
+++ helloworld.f95
@@ -0,0 +1,15 @@
+! 
+! 
+!                             Online Fortran Compiler.
+!                 Code, Compile, Run and Debug Fortran program online.
+! Write your code in this editor and press "Run" button to execute it.
+! 
+! 
+
+
+      program helloworld
+      implicit none
+      character*14 hello_string
+      hello_string = "HELLO WORLD!!!" !This is my first Fortran code.
+      write (*,*) hello_string
+      end program helloworld
